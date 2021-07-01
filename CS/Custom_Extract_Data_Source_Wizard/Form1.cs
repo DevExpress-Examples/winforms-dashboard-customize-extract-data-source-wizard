049ef5b0ad345cd729d57c826e73ebb6a12078c9
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2021-07-01 18:56:31 +0300

    Update Form1.cs

--- CS/Custom_Extract_Data_Source_Wizard/Form1.cs
+++ CS/Custom_Extract_Data_Source_Wizard/Form1.cs
@@ -1,4 +1,4 @@
-﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardCommon;
 using DevExpress.DashboardCommon.DataSourceWizard;
 using DevExpress.DashboardWin;
 using DevExpress.DashboardWin.DataSourceWizard;
@@ -6,45 +6,33 @@ using DevExpress.DataAccess.UI.Wizard;
 using DevExpress.XtraEditors;
 using System;
 
-namespace Custom_Extract_Data_Source_Wizard
-{
-    public partial class Form1 : XtraForm
-    {
-        public Form1()
-        {
+namespace Custom_Extract_Data_Source_Wizard {
+    public partial class Form1 : XtraForm {
+        public Form1() {
             InitializeComponent();
         }
 
-        private void btn_RunWizard_Click(object sender, EventArgs e)
-        {
+        private void btn_RunWizard_Click(object sender, EventArgs e) {
             EditExtractOptionsContext optionsContext = new EditExtractOptionsContext(this.GetActiveLookAndFeel(), this, dashboardViewer1.Dashboard.DataSources);
             optionsContext.WizardCustomization = new CustomExtractDataSourceWizardCustomizationService();
             ExtractDataSourceUIHelper.EditExtractOptions(new DashboardExtractDataSource() { FileName = "test.dat" }, optionsContext);
         }
     }
 
-    public class CustomExtractDataSourceWizardCustomizationService : IExtractDataSourceWizardCustomization
-    {
-        public void CustomizeDataSourceWizard(IWizardCustomization<ExtractDataSourceModel> customization)
-        {
+    public class CustomExtractDataSourceWizardCustomizationService : IExtractDataSourceWizardCustomization {
+        public void CustomizeDataSourceWizard(IWizardCustomization<ExtractDataSourceModel> customization) {
             customization.RegisterPageView<ICreateExtractDataSourcePageView, CustomConfigureExtractDataSourcePageView>();
         }
-
     }
 
-    public class CustomConfigureExtractDataSourcePageView : CreateExtractDataSourcePageView
-    {
-        public CustomConfigureExtractDataSourcePageView() : base()
-        {                     
-            this.checkEditExistingDataSource.Enabled = false;
-            
+    public class CustomConfigureExtractDataSourcePageView : CreateExtractDataSourcePageView {
+        public CustomConfigureExtractDataSourcePageView() : base() {                     
+            this.checkEditExistingDataSource.Enabled = false;            
         }
 
-        protected override void OnCheckEditExistingDataSourceCheckedChanged(object sender, EventArgs e)
-        {
+        protected override void OnCheckEditExistingDataSourceCheckedChanged(object sender, EventArgs e) {
             base.OnCheckEditExistingDataSourceCheckedChanged(sender, e);
             TextEditFileName.Enabled = false;
         }
-
     }
 }
